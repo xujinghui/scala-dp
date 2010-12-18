4a84beebc34193c2e17e1bc9a016e22fe7f010c2
stevenxu <stevenxu@stevenxu>
2010-12-18 07:50:28 +0000

    test

---
+++ src/org/ns/scala/JavaScala.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.ns.scala;
+
+/**
+ * @author Administrator
+ *
+ */
+public class JavaScala {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
