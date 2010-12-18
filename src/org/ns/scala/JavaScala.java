255c3c885024c0f6d04516c6bb09ef2640d15e96
stevenxu <stevenxu@stevenxu>
2010-12-18 07:56:56 +0000

    local sx

--- src/org/ns/scala/JavaScala.java
+++ src/org/ns/scala/JavaScala.java
@@ -13,7 +13,7 @@ public class JavaScala {
      * @param args
      */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	System.out.println("Scala. IN 123");
 
     }
 
